3ab4abb7547d7c93847ed853ee2bc099080817fb
Jerome Ghesquiere <jerome.ghesquiere@orange.com>
2019-10-03 11:20:27 +0200

    Add script

---
+++ Editor/FolderStructureGenerator.cs
@@ -0,0 +1,53 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class FolderStructureGenerator
+{
+    [MenuItem("Tools/FolderStructureGenerator/Generate folder structure")]
+    private static void GenerateFolderStructure()
+    {
+        GenerateFolder("Plugins");
+        GenerateFolder("Editor");
+        GenerateFolder("Project");
+        GenerateFolder("Resources/Prefabs/UI/Views");
+        GenerateFolder("Scenes");
+
+        GenerateFolder("Project/Fonts/Raws");
+
+        GenerateFolder("Project/Graphics/2D/UI");
+        GenerateFolder("Project/Graphics/3D");
+        GenerateFolder("Project/Graphics/Animations");
+        GenerateFolder("Project/Graphics/Materials/Particles");
+
+        GenerateFolder("Project/ScriptableObjects/UnityWeld/AdaptersOptions");
+
+        GenerateFolder("Project/Scripts/UI");
+        GenerateFolder("Project/Scripts/Utils");
+        GenerateFolder("Project/Scripts/ScriptableObjects");
+    }
+
+    private static void GenerateFolder(string link)
+    {
+        var root = "Assets";
+
+        var folders = link.Split('/');
+
+        if (folders.Length == 0)
+        {
+            folders = new string[] { link };
+        }
+
+        for (int i = 0; i < folders.Length; i++)
+        {
+            if (!AssetDatabase.IsValidFolder(root + "/" + folders[i]))
+            {
+                Debug.Log(root + " == " + folders[i]);
+            }
+
+            root += "/" + folders[i];
+        }
+    }
+}
+#endif
